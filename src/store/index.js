e9a8578a127484d05cc4f4823b8bd0b752d7052d
AyabulelaSityata <sityataayabulela@gmail.com>
2023-06-28 13:44:28 +0200

    if-mutation-added

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,26 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    counter: null
   },
   getters: {
   },
   mutations: {
+    addition(state) {
+      state.counter++
+    },
+    subtraction(state) {
+      if(state.counter > 0)
+      state.counter--
+    }
   },
   actions: {
+    increment(context) {
+      context.commit('addition')
+    },
+    decrement(context) {
+      context.commit('subtraction')
+    }
   },
   modules: {
   }
---
+++ src/views/CounterView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <h1>Counter</h1>
+        <div class="row">
+            <div class="col">
+                <button @click="increment" class="btn btn-success">+</button>
+            </div>
+            <div class="col counter">
+                <p class="lead">{{ counter }}</p>
+            </div>
+            <div class="col">
+                <button @click="decrement" class="btn btn-success">-</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            counter() {
+                return this.$store.state.counter
+            }
+        },
+        methods: {
+            increment() {
+                this.$store.dispatch('increment')
+            },
+            decrement() {
+                this.$store.dispatch('decrement')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.counter {
+    background-color: black;
+    color: white;
+    
+}
+
+</style>
